76470aa2e738fdb3c685973c1a1abe04f37b83a0
Anthony Salemo <ajssalemo@gmail.com>
2021-02-17 20:42:01 -0500

    added a GetMapping for finding by id

--- springbootauth0postgres/src/main/java/com/server/springbootauth0postgres/Controllers/ListingsAndReviewsController.java
+++ springbootauth0postgres/src/main/java/com/server/springbootauth0postgres/Controllers/ListingsAndReviewsController.java
@@ -2,11 +2,14 @@ package com.server.springbootauth0postgres.Controllers;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.server.springbootauth0postgres.Entities.ListingsAndReviews;
 import com.server.springbootauth0postgres.Repository.ListingsAndReviewsRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,10 @@ public class ListingsAndReviewsController {
     @GetMapping("/all_listings")
     public List<ListingsAndReviews> getAllListingsAndReviews() {
         return listingsAndReviewsRepostiory.findAll();
+    }
+
+    @GetMapping("/find_by_id/{id}")
+    public ListingsAndReviews findListingsAndReviewsByID(@PathVariable Long id) {
+        return listingsAndReviewsRepostiory.findById(id).orElseThrow(() -> new EntityNotFoundException("Cannot find the listing with id " + id));
     }
 }
