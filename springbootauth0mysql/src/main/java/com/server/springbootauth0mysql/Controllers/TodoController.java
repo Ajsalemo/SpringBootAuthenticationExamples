974c7ee6235957e52fd4914e3559e4daf0dda7cf
Anthony Salemo <ajssalemo@gmail.com>
2021-02-26 12:04:21 -0500

    inserts example data into the mysql data to use for the API

---
+++ springbootauth0mysql/src/main/java/com/server/springbootauth0mysql/Controllers/TodoController.java
@@ -0,0 +1,16 @@
+package com.server.springbootauth0mysql.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TodoController {
+
+    @GetMapping("/get_todo")
+    public String getTodos() {
+        return "This is a placeholder";
+    }
+}
