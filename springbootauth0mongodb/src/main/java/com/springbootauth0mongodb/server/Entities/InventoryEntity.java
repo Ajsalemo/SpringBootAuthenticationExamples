2b6c19af385dfad2642f501ed72d7adf6343a3b1
Anthony Salemo <ajssalemo@gmail.com>
2021-03-08 20:32:57 -0500

    able to connect to a local mongodb instance

---
+++ springbootauth0mongodb/src/main/java/com/springbootauth0mongodb/server/Entities/InventoryEntity.java
@@ -0,0 +1,45 @@
+package com.springbootauth0mongodb.server.Entities;
+
+import org.springframework.data.annotation.Id;
+
+public class InventoryEntity {
+
+    @Id
+    public String id;
+    
+    public String item;
+    public Integer qty;
+
+    public void Inventory () { 
+        // Empty method initialization
+    }
+
+    public void Inventory(String item, Integer qty) {
+        this.item = item;
+        this.qty = qty;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+}
