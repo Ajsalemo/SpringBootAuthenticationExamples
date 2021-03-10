b21ff38e449a1045ca94f02414c0a51a97a85b76
Anthony Salemo <ajssalemo@gmail.com>
2021-03-10 16:37:32 -0500

    added an endpoint to find by ID

--- springbootauth0mongodb/src/main/java/com/springbootauth0mongodb/server/Controllers/InventoryController.java
+++ springbootauth0mongodb/src/main/java/com/springbootauth0mongodb/server/Controllers/InventoryController.java
@@ -1,10 +1,13 @@
 package com.springbootauth0mongodb.server.Controllers;
 
+import java.util.Optional;
+
 import com.springbootauth0mongodb.server.Entities.InventoryEntity;
 import com.springbootauth0mongodb.server.Repository.InventoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +17,15 @@ public class InventoryController {
     @Autowired
     private InventoryRepository inventoryRepository;
 
+    // Retrieve all inventory
     @GetMapping("/find_all_inventory")
     public Iterable<InventoryEntity> findAllInventory() {
         return inventoryRepository.findAll();
+    }
+
+    // Find an item by ID
+    @GetMapping("/inventory/{id}")
+    public InventoryEntity findInventoryById(@PathVariable String id) throws Exception {
+        return inventoryRepository.findById(id).orElseThrow(() -> new Exception("Item not found with ID:" + id));
     }
 }
