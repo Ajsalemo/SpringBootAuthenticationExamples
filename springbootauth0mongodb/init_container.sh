68ac93398101ae78e4630f795600468c8fe99776
Anthony Salemo <ajssalemo@gmail.com>
2021-03-16 17:50:26 -0400

    able to hook up the containerized application to a Mongo Atlast Cluster successfully

--- springbootauth0mongodb/init_container.sh
+++ springbootauth0mongodb/init_container.sh
@@ -5,4 +5,4 @@ echo "Starting SSH ..."
 service ssh start
 
 echo "Running startup command 'java -jar /app/app.jar'"
-java -Dspring.data.mongodb.uri=mongodb://localhost:27017/test -jar /app/app.jar
+java -jar /app/app.jar
