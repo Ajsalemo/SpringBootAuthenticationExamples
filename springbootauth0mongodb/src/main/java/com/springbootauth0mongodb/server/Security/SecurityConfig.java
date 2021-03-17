ddd1573ebbc07b87c48b68a5849deba761951a4b
Anthony Salemo <ajssalemo@gmail.com>
2021-03-17 17:23:25 -0400

    added Swagger for REST documentation

--- springbootauth0mongodb/src/main/java/com/springbootauth0mongodb/server/Security/SecurityConfig.java
+++ springbootauth0mongodb/src/main/java/com/springbootauth0mongodb/server/Security/SecurityConfig.java
@@ -23,6 +23,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // Allow Swagger to be accessed publicly
+        http.authorizeRequests().antMatchers("/v2/api-docs", "/swagger-ui/**", "/swagger-resources",
+                "/swagger-resources/configuration/ui", "/swagger-resources/configuration/security").permitAll();
+
         // Only allow GET requests to '/' to be permitted
         http.authorizeRequests().mvcMatchers(HttpMethod.GET, "/").permitAll().anyRequest().authenticated().and()
                 .oauth2ResourceServer().jwt();
