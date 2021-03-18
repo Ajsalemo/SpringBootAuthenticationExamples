31f2ba76ffd3b74337d1ee2e1b66860ca9cf40e6
Anthony Salemo <ajssalemo@gmail.com>
2021-03-18 12:17:47 -0400

    put the HomeController back up /Controllers

--- springbootauth0mongodb/src/main/java/com/springbootauth0mongodb/server/Springboothauth0mongodbApplication.java
+++ springbootauth0mongodb/src/main/java/com/springbootauth0mongodb/server/Springboothauth0mongodbApplication.java
@@ -2,21 +2,11 @@ package com.springbootauth0mongodb.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication()
-@RestController
-@RequestMapping("/")
 public class Springboothauth0mongodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboothauth0mongodbApplication.class, args);
-	}
-
-	@GetMapping
-	public String home() {
-		return "Springbootauth0mongodb";
 	}
 }
